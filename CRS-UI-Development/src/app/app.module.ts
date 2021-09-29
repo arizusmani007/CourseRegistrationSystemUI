b412899e6495a7dabffa76f627b35991f8a8e415
arizusmani007 <arizusmani007@gmail.com>
2021-09-29 23:00:55 +0530

    initial component and model

--- CRS-UI-Development/src/app/app.module.ts
+++ CRS-UI-Development/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './component/student/student.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { ProfessorComponent } from './component/professor/professor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    AdminComponent,
+    ProfessorComponent
   ],
   imports: [
     BrowserModule,
